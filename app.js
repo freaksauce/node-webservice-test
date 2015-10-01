01038fc5f659437b22e283a918b973bbddc45528
Jon Bloomer <jon@freaksauce.com>
2015-10-01 22:42:13 +1000

    commented code

--- app.js
+++ app.js
@@ -2,28 +2,31 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 
-var showsObj = {"response": []};
-var shows = [];
+var showsObj = {"response": []}; // obj to return as JSON
+var shows = []; // var to store filtered JSON
 
 // parse application/json
 app.use(bodyParser.json());
 
 app.use(function (req, res) {
 
+  // get JSON
   var showsJSON = req.body.payload;
+  // loop each object
   showsJSON.forEach(function(item) {
-    if (item.drm) {
-      if (item.episodeCount > 0) {
+    if (item.drm == true) { // requirement, drm must be true
+      if (item.episodeCount > 0) { // requirement, episodeCount > 0
         shows.push({"image": item.image.showImage,"slug": item.slug, "title": item.title});
       }
     }
   });
 
-  showsObj.response = shows;
-  res.end(JSON.stringify(showsObj, null, 2));
+  showsObj.response = shows; // add shows array
+  res.end(JSON.stringify(showsObj, null, 2)); // convert to JSON
 
 });
 
+// error handling
 app.use(function(err, req, res, next) {
     res.status(err.status || 400);
     res.send({"error": "Could not decode request: JSON parsing failed"});
